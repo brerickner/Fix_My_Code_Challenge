c1c513fc1b2960e4c8bb6e37f029c5cd3382e55b
brerickner <breannarickner@gmail.com>
2021-04-12 02:17:23 +0000

    is this working?

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -5,7 +5,7 @@ The size of the square must be the first argument
 of the program.
 */
 
-if (process.argv.length > 2) {
+if (process.argv.length <= 2) {
   process.stderr.write('Missing argument\n');
   process.stderr.write('Usage: ./1-print_square.js <size>\n');
   process.stderr.write('Example: ./1-print_square.js 8\n');
