c4644a0b984c3a32f92ff3fff636ca4e4c9097ba
Bre <49533377+brerickner@users.noreply.github.com>
2021-04-11 20:26:18 -0500

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,15 +6,13 @@ of the program.
 */
 
 if (process.argv.length > 2) {
-  debugger;
-} else {
   process.stderr.write('Missing argument\n');
   process.stderr.write('Usage: ./1-print_square.js <size>\n');
   process.stderr.write('Example: ./1-print_square.js 8\n');
   process.exit(1);
 }
 
-const size = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2], 10);
 
 for (let i = 0; i < size; i++) {
   for (let j = 0; j < size; j++) {
