924de76ae641a2f683b2e764b27f0dcfe90621b6
brerickner <breannarickner@gmail.com>
2021-05-19 21:03:42 +0000

    Adding square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
 """ This is a square module """
-class square():
-    """ This is a square method 
-    width = 0
-    height = 0"""
 
-    
-    def __init__(self, *args, **kwargs):
+
+class Square():
+    """ This is a square method """
+
+    width = 0
+    height = 0
+
+
+    def __init__(self, width, height):
         """ init of square """
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.height = height
+        self.width = width
+            
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         """ Method to get perimeter of square """
-        return (self.width * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ str repr method"""
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
